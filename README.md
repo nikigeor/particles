b1f340b45559256a43c0006bdfd5d16afd0d1625
Nikita George <nikkigeorge2@gmail.com>
2021-05-03 13:13:02 -0600

    react

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+https://www.npmjs.com/package/react-particles-js
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Particles
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 150
+	        },
+	        "size": {
+	            "value": 3
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        }
+	    }
+	}} />
+  </div>
   );
 }
+
 
 export default App;
